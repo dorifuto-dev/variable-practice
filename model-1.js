6784e308bb034cb9288060ecf2cb74d84622ccff
Eric Li <ericli@knights.ucf.edu>
2021-03-30 15:18:34 -0600

    Add strings to model-1.js

---
+++ model-1.js
@@ -0,0 +1,5 @@
+// Variables added to strings can include letters, numbers, and phrases, surrounded by quote
+// marks. For example:
+
+var whatColor = "red"
+var didGoToSchool = "did not go to school"
