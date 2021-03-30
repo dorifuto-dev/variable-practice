87a321d0628a6a195a7e28eea54482d58b5e8224
Eric Li <ericli@knights.ucf.edu>
2021-03-30 15:21:22 -0600

    Add numbers to model-1.js

--- model-1.js
+++ model-1.js
@@ -1,5 +1,11 @@
-// Variables added to strings can include letters, numbers, and phrases, surrounded by quote
+// Variables containing strings can include letters, numbers, and phrases, surrounded by quote
 // marks. For example:
 
 var whatColor = "red"
 var didGoToSchool = "did not go to school"
+
+// Varibles containing numbers can be positive, negative, or a decimal. For example:
+
+var numberDogs = 25
+var dealsForTheMonth = 22.5
+var temp = -20
